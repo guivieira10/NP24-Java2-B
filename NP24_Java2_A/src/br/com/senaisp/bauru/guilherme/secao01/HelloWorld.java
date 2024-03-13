b79eaeeadadcdaea7776509b5e7a42f14621ac07
guivieira10 <guivsantos2000@gmail.com>
2024-03-13 20:04:27 -0300

    Commit inicial

---
+++ NP24_Java2_A/src/br/com/senaisp/bauru/guilherme/secao01/HelloWorld.java
@@ -0,0 +1,9 @@
+package br.com.senaisp.bauru.guilherme.secao01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Olá mundo!");
+	}
+
+}
---
+++ NP24_Java2_A/src/br/com/senaisp/bauru/guilherme/secao01/RevisaoParametro.java
@@ -0,0 +1,11 @@
+package br.com.senaisp.bauru.guilherme.secao01;
+
+public class RevisaoParametro {
+	public static void main(String[] args) {
+		for(String item : args) 
+		{
+			System.out.println(item);
+		}
+	}
+
+}
