eb8cec440c893988671611072ec2ba938494b0d6
guivieira10 <guivsantos2000@gmail.com>
2024-03-18 22:04:11 -0300

    Conteúdo da aula do dia 18/03/2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/secao03/ShoppingCart01.java
@@ -0,0 +1,15 @@
+package br.com.senaisp.bauru.guilherme.secao03;
+
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+    	String custName = "Guilherme";
+    	String itemDesc = "Camisa";
+
+    	// Assign the message variable 
+        String message = custName + " comprou " + 1 + " " + itemDesc;
+        
+        // Print and run the code
+        System.out.println(message);
+    }
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/secao04/ConversaoTipos02.java
@@ -0,0 +1,12 @@
+package br.com.senaisp.bauru.guilherme.secao04;
+
+public class ConversaoTipos02 {
+
+	public static void main(String[] args) {
+		byte num1 = 127;
+		num1++;
+		System.out.println(num1);
+		System.out.println((int)3.5);
+	}
+
+}
