e2215ef9cf55f74ab57eeb7ab1449d3579909e5c
guivieira10 <guivsantos2000@gmail.com>
2024-04-01 20:31:01 -0300

    Conteúdo da aula do dia 01/04/2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/licao08/exercicios/TesteBaralho.java
@@ -0,0 +1,11 @@
+package br.com.senaisp.bauru.guilherme.licao08.exercicios;
+
+public class TesteBaralho {
+
+	public static void main(String[] args) {
+		Baralho br = new Baralho();
+		System.out.println(br.getCartaAleatoria());
+		System.out.println(br.getCartaAleatoria());
+	}
+
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/licao08/exercicios/TesteCarta.java
@@ -0,0 +1,10 @@
+package br.com.senaisp.bauru.guilherme.licao08.exercicios;
+
+public class TesteCarta {
+
+	public static void main(String[] args) {
+		Carta ct = new Carta (3,11);
+		System.out.println(ct);
+	}
+
+}
