0b9c089b597136b3dcfbd59d9337dad468ea97a9
guivieira10 <guivsantos2000@gmail.com>
2024-04-01 21:32:42 -0300

    Conteúdo da aula do dia 28/03/2024

--- NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/licao08/exercicios/JogoBlackJack.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/guilherme/licao08/exercicios/JogoBlackJack.java
@@ -33,6 +33,44 @@ public class JogoBlackJack {
 		ct = br.getCartaAleatoria();
 		score2 += ct.getValor();
 		System.out.println(ct);
+		//Verificando com o jogador 1 se deseja mais cartas
+		System.out.println("Hora do player 1 receber cartas...");
+		String cond = "S";
+		while(cond.equals("S")) {
+			System.out.println("Deseja mais cartas? (S/N)");
+			cond = sc.nextLine();
+			//Se sim, sorteamos uma carta
+			if (cond.equals("S")) {
+				ct = br.getCartaAleatoria();
+				score1 += ct.getValor();
+				System.out.println(ct);
+			}
+		}
+		//Agora vamos ao jogador 2
+		System.out.println("Hora do player 2 receber cartas...");
+		do {
+			System.out.println("Deseja mais cartas? (S/N)");
+			cond = sc.nextLine();
+			
+			//Se sim, sorteamos uma carta
+			if (cond.equals("S")) {
+				ct = br.getCartaAleatoria();
+				score2 += ct.getValor();
+				System.out.println(ct);
+			}
+			
+		}while (cond.equals("S"));
+		//Hora do vencedor!!
+		if (score1 == score2) {
+			System.out.println("Empate !!!");
+		} else if (score1>score2 && score1>22) {
+			System.out.println(jogador1 + " você é o vencedor!");
+		} else if (score2>score1 && score2<22) {
+			System.out.println(jogador2 + " você é o vencedor!");
+		} else {
+			System.out.println("Ninguem venceu!!");
+		}
+		//Fechando o scanner
+		sc.close();
 	}
-
 }
